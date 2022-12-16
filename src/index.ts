53bb658b802135258eac9f26c057eefee774db6a
Carmen Lopez <carmenbaez2000@gmail.com>
2022-12-16 21:40:31 +0100

    Summary
    - Design patterns
    - Testing
    - Debugging
    - Etc.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 // LECTURE 1 & 2: INSTALL & BASICS
 
+import { AsyncLocalStorage } from "async_hooks";
+
 console.log("Hola TypeScript!");
 
 // Comments are like JS
@@ -204,3 +206,142 @@ function buildNameRest(firstName: string, ...restOfName: string[]) {
 buildNameRest("Jose", "Pedro", "Natasha", "Maria");
 
 let buildNameFun: (fname: string, ...rest: string[]) => string = buildName;
+
+// Function overload
+// Use the same name for a function, but ask for different arguments
+
+function showError(error: string): void {
+  console.log("Error: " + error);
+}
+
+/**
+  function showError(error: string, code: number): void {
+  console.log('Error': error);
+
+  The system will decide which one to use based on arguments.
+  This approach is possible and useful in some cases, but 
+  generally it's recommended to create one function that allows
+  different arguments.
+ */
+
+/// LECTURE 4: HOW TO HANDLE DATA
+/// LocalStorage - store data in browser, not deleted automatically
+/// SessionStorage - store data while session
+/// Cookies - has expiration date
+
+function setLocalStorage() {
+  let x = "Lorem ipsum";
+  localStorage.set("data", x); // setItem for JS
+}
+
+function readLocalStorage() {
+  let value = localStorage.get("data");
+  console.log(value);
+}
+
+/// How to work with cookies
+/// https://www.npmjs.com/package/cookies-utils
+
+import { setCookie } from "cookies-utils";
+
+const cookieOptions = {
+  name: "name", // string,
+  value: "value", // string,
+  maxAge: 10 * 60, // optional number (value in seconds),
+  expires: new Date(2022, 12, 31), // optional Date,
+  path: "/path", // optional string,
+  domain: "site.com", // optional string,
+  secure: true, // optional boolean,
+  sameSite: "lax", // optional enum 'lax' | 'strict' | 'none'
+};
+
+setCookie(cookieOptions);
+deleteCookie("name"); // Name given to cookieOptions
+
+const hasValue = cookieHasValue("name", "value");
+console.log(hasValue);
+
+deleteAllCookies();
+
+/// Classes + Time
+
+class Temporizador {
+  public terminar: () => void;
+
+  public empezar(): void {
+    setTimeout(() => {
+      if (!this.terminar) return;
+    }, 1000);
+  }
+}
+
+const miTemporizador: Temporizador = new Temporizador();
+
+miTemporizador.terminar = () => {
+  console.log("Ha terminado");
+};
+
+setInterval(() => console.log("Tic"), 1000);
+
+// delete miTemporizador.terminar para terminar un evento y que no se queden ejecutando
+
+// LECTURE 5, 6 & 7: CLASSES, OBJECTS, HERITAGE, POLIMORFISM, DECORATORS, INTERFACES
+
+class Estudiantes {
+  public nombre: string;
+  public apellidos: string;
+
+  constructor(nombre: string, apellidos?: string) {
+    this.nombre = nombre;
+    this.apellidos = apellidos | undefined;
+  }
+}
+
+const miEstudiante: Estudiantes = new Estudiantes("Maria", "Lopez");
+console.log(miEstudiante);
+
+// Saber la instancia de un objeto
+// typeOF
+// instanceOf
+
+const misEstudiantes: Estudiantes[] = [];
+misEstudiantes.push("Pedro", "Barrios");
+misEstudiantes.push("Pablo", "Casals");
+misEstudiantes.push("Juan", "Rondon");
+
+misEstudiantes.forEach((alumno) => {
+  console.log(alumno);
+})
+
+class Estudiantes_Superiores extends Estudiantes {
+
+  public micurso: number;
+
+  constructor(nombre: string, apellidos?: string, curso: number) {
+    super(nombre, apellidos);
+    this.micurso: curso;
+  }
+}
+
+
+/// DEVELOPMENT DESIGN STRUCTURE --> How to organize a project?
+
+/**
+ * 
+ * - When you use a framework, you can use the standard of the framework
+ * 
+ * CREATIONAL
+ * - Objects
+ * - Reutilization of code
+ * - Flexibility
+ * 
+ * STRUCTURAL
+ * - Eficiency and flexibility of the structure
+ * 
+ * BEHAVIORAL
+ * - Behavior-based structure (one structure - one task)
+ * 
+ * 
+ * Sample: https://refactoring.guru/design-patterns/catalog
+ * 
+ */
